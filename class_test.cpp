9fcf53e83c2ccbb6e4f6ca61c43efd63dc1c008c
aewhitfield90 <aewhitfield90@gmail.com>
2022-02-25 18:35:32 -0800

    updated my branch to match task.

---
+++ class_test.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include "creature.cpp"
+using namespace std;
+
+int main() {//tester main for classes
+	cout << "Welcome to Text RPG" << endl;//dafsfasdf
+	cout << "Creating Player 1" << endl;//dafsfasdf
+	Player player1("Tester");
+
+	player1.getPlayerStats();
+
+	cout << "Changing " << player1.getName() << " name to Steve" << endl << endl;
+	player1.setName("Steve");
+
+
+	player1.getPlayerStats();
+
+	return 0;
+}
+
